56d77957d167c94d3622e563dad131fbe73964ff
Hero <hero@Heros-MacBook-Pro.local>
2023-03-26 14:57:07 +0900

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,48 @@
-# Homebrew
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Zsh
-chsh -s `which zsh`
-touch ~/.zshrc
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# dotfiles
-git clone https://github.com/mu29/dotfiles
-cp ~/Github/dotfiles/.zshrc ~/.zshrc
-cp ~/Github/dotfiles/.vimrc ~/.vimrc
-cp ~/Github/dotfiles/mu29.zsh-theme ~/.oh-my-zsh/themes/mu29.zsh-theme
-sh ~/Github/dotfiles/fonts/install.sh
-
 # Github
 cd ~
 mkdir Github
 
+# Clone dotfiles
+cd ~/Github
+git clone https://github.com/mu29/dotfiles
+sh dotfiles/fonts/install.sh
+cp dotfiles/mu29.zsh-theme ~/.oh-my-zsh/themes/mu29.zsh-theme
+cp dotfiles/.zshrc ~/.zshrc
+source ~/.zshrc
+cd ~
+
 # rbenv
 brew install rbenv
-rbenv init
+echo '' >> ~/.zshrc
+echo '# rbenv' >> ~/.zshrc
+echo 'eval "$(~/.rbenv/bin/rbenv init - zsh)"' >> ~/.zshrc
+source ~/.zshrc
+
+rbenv install 2.7.6
+rbenv global 2.7.6
+rbenv rehash
+
+# Cocoapods
+gem install cocoapods
 
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-nvm install v12.20.1
-curl -o- -L https://yarnpkg.com/install.sh | bash
+brew install nvm
+mkdir ~/.nvm
+echo '' >> ~/.zshrc
+echo '# nvm' >> ~/.zshrc
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.zshrc
+echo '  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
+source ~/.zshrc
+
+nvm install 16.19.1
